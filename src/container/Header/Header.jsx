b4ecb28651fc5434f49f183fff5c5706e998ee88
Jeffrey Ronald <TheBlueAstronomer@users.noreply.github.com>
2023-09-25 00:56:47 +0530

    Navbar complete with bits of the hero section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { easeIn, motion } from 'framer-motion'
 import { images } from '../../constants'
 
 import './Header.scss'
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 function Header() {
   return (
-    <div className='app__header app__flex'>
-      
+    <div className='app__header app__flex' id = 'home'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp'>
+              <p className='p-text'>Hello there, Nice to meet you ❤️, I AM</p>
+              <h1 className='head-text'>Hannah</h1>
+          </div>
+          <div className='tag-cmp '>
+            <p className='p-text'>Freelance Writer and film critic with a history of working in the publishing industry.</p>
+
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg" />
+      </motion.div>
+
     </div>
   )
 }
